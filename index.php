436c66d89391f632afdaf6c5378109a5ede252b7
Daniel0413 <renzdaniel111304@gmail.com>
2026-02-19 15:55:17 +0800

    Update access token and improve message handling

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-
 <?php
 // CONFIGURATION
-// Replace these with your actual tokens later
-$access_token = 'EAAbrMgvNBJABQD71Kpe2HRZCvXmqrzoYKSPM4GelWIBbnZByejCNmqfBFYDqjILtHd2JO91YcZAelUwOcXFsjLgE0k7WUY2jx6SZCAyglRT43DYumcFOLxIZCrtTKeEUzj5If8tINqmi3HVBP0BdP0GwpaDuY4nuQQ75x2ZChyZBo43GsJiVqVXAKxr5x1KU4aIlndZBeQZDZD'; 
+// 1. REGENERATE your token in Facebook Developers and paste the NEW one here.
+$access_token = 'REPLACE_THIS_WITH_YOUR_NEW_ACCESS_TOKEN'; 
 $verify_token = 'Moz1304'; 
 
 // 1. WEBHOOK VERIFICATION
@@ -23,14 +22,23 @@ $data = json_decode($input, true);
 
 if ($data && isset($data['entry'][0]['messaging'][0])) {
     $event = $data['entry'][0]['messaging'][0];
-    $sender_id = $event['sender']['id'];
-
-    if (isset($event['message']['text'])) {
-        // Get Name
-        $user_info = json_decode(file_get_contents("https://graph.facebook.com/$sender_id?fields=first_name,last_name&access_token=$access_token"), true);
+    
+    // Check if it is a text message and has a sender
+    if (isset($event['sender']['id']) && isset($event['message']['text'])) {
+        $sender_id = $event['sender']['id'];
         
-        $name = isset($user_info['first_name']) ? $user_info['first_name'] . " " . $user_info['last_name'] : "Friend";
-        $reply = "Hello $name! ";
+        // Get the Chatter's Name
+        // Note: This requires your App to be in "Live" mode to work for the public.
+        $user_info_json = file_get_contents("https://graph.facebook.com/$sender_id?fields=first_name,last_name&access_token=$access_token");
+        $user_info = json_decode($user_info_json, true);
+        
+        // If we can't get the name (e.g., app in dev mode), default to "Friend"
+        $chatter_name = isset($user_info['first_name']) ? $user_info['first_name'] : "Friend";
+        
+        // --- THIS IS THE SECTION YOU WANTED TO CHANGE ---
+        // It greets the chatter, then introduces you.
+        $reply = "Hi $chatter_name! I'm Renz Daniel A. Rafael."; 
+        // ------------------------------------------------
 
         // Send Reply
         $options = [
@@ -43,7 +51,9 @@ if ($data && isset($data['entry'][0]['messaging'][0])) {
                 ])
             ]
         ];
-        file_get_contents("https://graph.facebook.com/v18.0/me/messages?access_token=$access_token", false, stream_context_create($options));
+        
+        // Use @ to suppress warnings if the HTTP request fails
+        @file_get_contents("https://graph.facebook.com/v18.0/me/messages?access_token=$access_token", false, stream_context_create($options));
     }
 }
 http_response_code(200);
