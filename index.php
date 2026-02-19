fd3dc351e76de56bdcfa233436e805960353bce3
Daniel0413 <renzdaniel111304@gmail.com>
2026-02-19 16:09:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
+
+
+
 <?php
 // CONFIGURATION
-// 1. REGENERATE your token in Facebook Developers and paste the NEW one here.
+// ⚠️ WARNING: You posted your token publicly again. 
+// Please Generate a NEW Page Access Token immediately in the FB Developer Dashboard.
 $access_token = 'EAAbrMgvNBJABQD71Kpe2HRZCvXmqrzoYKSPM4GelWIBbnZByejCNmqfBFYDqjILtHd2JO91YcZAelUwOcXFsjLgE0k7WUY2jx6SZCAyglRT43DYumcFOLxIZCrtTKeEUzj5If8tINqmi3HVBP0BdP0GwpaDuY4nuQQ75x2ZChyZBo43GsJiVqVXAKxr5x1KU4aIlndZBeQZDZD'; 
 $verify_token = 'Moz1304'; 
 
@@ -23,22 +27,26 @@ $data = json_decode($input, true);
 if ($data && isset($data['entry'][0]['messaging'][0])) {
     $event = $data['entry'][0]['messaging'][0];
     
-    // Check if it is a text message and has a sender
     if (isset($event['sender']['id']) && isset($event['message']['text'])) {
         $sender_id = $event['sender']['id'];
         
-        // Get the Chatter's Name
-        // Note: This requires your App to be in "Live" mode to work for the public.
-        $user_info_json = file_get_contents("https://graph.facebook.com/$sender_id?fields=first_name,last_name&access_token=$access_token");
-        $user_info = json_decode($user_info_json, true);
+        // --- SAFE NAME FETCHING ---
+        // Default to "Friend" first
+        $chatter_name = "Friend";
         
-        // If we can't get the name (e.g., app in dev mode), default to "Friend"
-        $chatter_name = isset($user_info['first_name']) ? $user_info['first_name'] : "Friend";
+        // We use '@' to suppress the error if Facebook blocks the request
+        $user_info_json = @file_get_contents("https://graph.facebook.com/$sender_id?fields=first_name&access_token=$access_token");
         
-        // --- THIS IS THE SECTION YOU WANTED TO CHANGE ---
-        // It greets the chatter, then introduces you.
+        // Only update the name if the request was actually successful
+        if ($user_info_json) {
+            $user_info = json_decode($user_info_json, true);
+            if (isset($user_info['first_name'])) {
+                $chatter_name = $user_info['first_name'];
+            }
+        }
+        // --------------------------
+
         $reply = "Hi $chatter_name! I'm Renz Daniel A. Rafael."; 
-        // ------------------------------------------------
 
         // Send Reply
         $options = [
@@ -48,11 +56,11 @@ if ($data && isset($data['entry'][0]['messaging'][0])) {
                 'content' => json_encode([
                     'recipient' => ['id' => $sender_id],
                     'message' => ['text' => $reply]
-                ])
+                ]),
+                'ignore_errors' => true // Prevent crashing on send error
             ]
         ];
         
-        // Use @ to suppress warnings if the HTTP request fails
         @file_get_contents("https://graph.facebook.com/v18.0/me/messages?access_token=$access_token", false, stream_context_create($options));
     }
 }
